a3f1277ed6c741ee0750ad04c0c18dc52adde43d
Ilia Ilmer <i.ilmer@icloud.com>
2020-03-29 21:18:29 -0400

    add sloppy tests, should simplify later

--- test.py
+++ test.py
@@ -1,10 +1,31 @@
 import pytest
-from memory import MemoryFIFO, MemoryFIFO2
+from memory import MemoryFIFO, MemoryFIFO2, Memory
+from numpy.random import randint
+from typing import Union
 
 
-def testFIFO():
-    fifo = MemoryFIFO
+def fill(M: MemoryFIFO) -> MemoryFIFO:
+    vals = randint(-10, 10, size=M.size)
+    for v in vals:
+        M.add(v)
+    return M
 
 
-def testFIFO2():
-    fifo = MemoryFIFO2
+@pytest.mark.parametrize("size", [10, 20, 50])
+def testFIFO(size):
+    fifo = MemoryFIFO(5)
+    fifo.storage = [1, 2, 3, 4, 5]
+    item = fifo.get()
+    assert item == 1
+    assert fifo.storage == [2, 3, 4, 5]
+    fifo = MemoryFIFO(size)  # no eviction
+    fifo = fill(fifo)
+    with pytest.raises(ValueError):
+        fifo.add(10)
+    with pytest.raises(ValueError):
+        fifo = MemoryFIFO(size)
+        fifo.get()
+
+
+# def testFIFO2():
+#     fifo = MemoryFIFO2
